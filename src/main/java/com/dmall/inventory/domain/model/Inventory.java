0169ba8e92247394bc2a9a43a3dd94614bb56805
linwenjun <linwenjun@163.com>
2017-12-30 23:48:42 +0800

    change config of application

--- src/main/java/com/dmall/inventory/domain/model/Inventory.java
+++ src/main/java/com/dmall/inventory/domain/model/Inventory.java
@@ -1,12 +1,10 @@
 
 package com.dmall.inventory.domain.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
+@Table(name = "inventory")
 public class Inventory {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
