498fe25edd3139ace59df30d19f7a86fabae31c5
linwenjun <linwenjun@163.com>
2018-01-01 02:19:45 +0800

    change migration script

--- src/main/resources/db/migration/V2__Alter_Inventory_table_column_name.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE `inventory` CHANGE COLUMN `currentQuantity` `current_quantity` BIGINT NOT NULL;
-ALTER TABLE `inventory` CHANGE COLUMN `lockedQuantity` `locked_quantity` BIGINT NOT NULL;
--- src/test/resources/db/migration/V2_1__Add_data_for_Inventory.sql
+++ src/test/resources/db/migration/V2_1__Add_data_for_Inventory.sql
@@ -1,1 +1,1 @@
-INSERT INTO `inventory` VALUES (1, 'aaaa-bbbb-cccc', 'A1', 10000, 100, '2012-09-17 18:47:52.69');
+INSERT INTO `inventory` VALUES (null, 'aaaa-bbbb-cccc', 'A1', 10000, 100, '2012-09-17 18:47:52.69');
