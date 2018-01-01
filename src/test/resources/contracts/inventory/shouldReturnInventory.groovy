99610aeb3d6b9e063b6acc79d745293e1d3be13b
linwenjun <linwenjun@163.com>
2018-01-02 00:50:47 +0800

    change contract

--- src/test/resources/contracts/inventory/shouldReturnInventory.groovy
+++ src/test/resources/contracts/inventory/shouldReturnInventory.groovy
@@ -7,15 +7,15 @@ import org.springframework.cloud.contract.spec.Contract
             name('Should get a product')
             request {
                 method GET()
-                url ('/inventories/iPhone%20X%2064G%20%E9%BB%91%E8%89%B2')
+                url ('/inventories/6009907')
             }
             response {
                 status 200
                 body("""
                     {
                         "id": 1,
-                        "sku": "iPhone X 64G 黑色",
-                        "warehouse": "成都仓",
+                        "sku": "6009907",
+                        "warehouse": "北京仓",
                         "currentQuantity": 100,
                         "lockedQuantity": 0,
                         "availableQuantity": 100
