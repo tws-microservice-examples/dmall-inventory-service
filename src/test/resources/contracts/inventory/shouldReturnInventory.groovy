9a2c08e75eb06603b70210f918bb81da183edd98
linwenjun <linwenjun@163.com>
2018-01-02 18:28:49 +0800

    chaneg test

--- src/test/resources/contracts/inventory/shouldReturnInventory.groovy
+++ src/test/resources/contracts/inventory/shouldReturnInventory.groovy
@@ -8,9 +8,15 @@ import org.springframework.cloud.contract.spec.Contract
             request {
                 method GET()
                 url ('/inventories/6009907')
+                headers {
+                    header('''Content-Type''', '''application/json;charset=UTF-8''')
+                }
             }
             response {
                 status 200
+                headers {
+                    header('''Content-Type''', '''application/json;charset=UTF-8''')
+                }
                 body("""
                     {
                         "id": 1,
@@ -23,7 +29,45 @@ import org.springframework.cloud.contract.spec.Contract
                 """)
                 testMatchers {
                     jsonPath('$.id', byRegex(number()))
+                    jsonPath('$.lockedQuantity', byRegex(number()))
+                    jsonPath('$.availableQuantity', byRegex(number()))
                 }
             }
         },
+
+        Contract.make {
+            name('Should return product when post a lock event')
+            request {
+                method POST()
+                url ('/inventories/6009907/lockEvents')
+                headers {
+                    header('''Content-Type''', '''application/json;charset=UTF-8''')
+                }
+                body("""
+                    {
+                        "sku": "6009907",
+                        "quantity": 1
+                    }
+                """)
+            }
+            response {
+                status 201
+                headers {
+                    header('''Content-Type''', '''application/json;charset=UTF-8''')
+                }
+                body("""
+                    {
+                        "id": 1,
+                        "sku": "6009907",
+                        "warehouse": "北京仓",
+                        "currentQuantity": 100,
+                        "lockedQuantity": 1,
+                        "availableQuantity": 99
+                    }
+                """)
+                testMatchers {
+                    jsonPath('$.id', byRegex(number()))
+                }
+            }
+        }
 ]
